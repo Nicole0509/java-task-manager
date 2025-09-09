8d980a73f26408a6e4850407619647162a5ca1ec
Nicole0509 <niashavril0509@gmail.com>
2025-09-09 13:59:46 +0200

    displaying tasks

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a task manager");
+
+        String[] tasks = {"Task1", "Task2"};
+
+        for(String task : tasks){
+            System.out.println(task);
+        }
+    }
+}
