65983f8e794a75695513f27081f02a8093efc49a
Nicole0509 <niashavril0509@gmail.com>
2025-09-18 18:46:35 +0200

    Created a switch menu for the user to choose an option and accepted input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,44 @@
 package org.example;
 
-import task.Task;
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a task manager\n");
 
-        ArrayList<Task> Tasks = new ArrayList<Task>();
+        Scanner scanner = new Scanner(System.in);
 
-        Tasks.add(new Task("Learning java!"));
-        Tasks.add(new Task("Learning SSH!"));
-        Tasks.add(new Task("Learning React Js!"));
+        System.out.println("Welcome to your task manager\n");
 
-        for(Task task : Tasks){
-            System.out.println(task);
+        System.out.println("What do you want to do next?");
+        System.out.println("1. View Tasks");
+        System.out.println("2. Add Task");
+        System.out.println("3. Update Task");
+        System.out.println("4. Delete Task");
+        System.out.println("5. Exit");
+
+        System.out.print("\nChoose an option: ");
+        int choice =  scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("View Tasks");
+                break;
+            case 2:
+                System.out.println("Add Task");
+                break;
+            case 3:
+                System.out.println("Update Task");
+                break;
+            case 4:
+                System.out.println("Delete Task");
+                break;
+            case 5:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
         }
     }
 }
