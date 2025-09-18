53b814683dbd5639763b5b7c6877e0324d5581ff
Nicole0509 <niashavril0509@gmail.com>
2025-09-18 14:27:39 +0200

    Created my first arry list and named it task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a task manager");
 
-        String[] tasks = {"Task1", "Task2"};
+        ArrayList<String> Tasks = new ArrayList<String>();
 
-        for(String task : tasks){
-            System.out.println(task);
-        }
+
     }
 }
