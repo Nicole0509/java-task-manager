3b811e511f50d4bb712c8e77d483fe6c2cbb2ef9
Nicole0509 <niashavril0509@gmail.com>
2025-09-18 14:38:39 +0200

    Allowed user to add on a task on the list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is a task manager");
+        System.out.println("This is a task manager\n");
+
+        Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> Tasks = new ArrayList<String>();
 
@@ -15,10 +18,24 @@ public class Main {
 
         int i = 0;
 
+        System.out.println("Your current tasks:");
+
         for(String task : Tasks){
             ++i;
             System.out.println("Task number " + i +": " + task);
         }
 
+        System.out.println();
+
+        System.out.println("Please enter the number of items you want to add: ");
+        String newTask = scanner.nextLine();
+        System.out.println(newTask);
+
+        Tasks.add(newTask);
+
+        for(String task : Tasks){
+            ++i;
+            System.out.println("Task number " + i +": " + task);
+        }
     }
 }
